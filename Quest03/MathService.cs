585082ba0fae1653859184d7dd9042ecc353874d
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-07-17 03:09:53 +0500

    Fix 3

--- Quest03/MathService.cs
+++ Quest03/MathService.cs
@@ -6,10 +6,16 @@ namespace Quest03
 {
     class MathService
     {
-        public int GetDistance(int numberMin, int numberMax)
+        public int GetCathetus(int numberMin, int numberMax)
         {
             int result = numberMax - numberMin;
             return result;
+        }
+        public double GetHypotenuse(double sideOne, double sideTwo)
+        {
+            double resOne = (sideOne) * (sideOne) + (sideTwo) * (sideTwo);
+            double resTwo = Math.Sqrt(resOne);
+            return resTwo;
         }
     }
 }
--- Quest03/Program.cs
+++ Quest03/Program.cs
@@ -6,21 +6,41 @@ namespace Quest03
     {
         static void Main(string[] args)
         {
-            Point point = new Point();
-            point.x = int.Parse(Console.ReadLine());
-            point.y = int.Parse(Console.ReadLine());
-            if (point.x >= point.y)
+            Point pointOne = new Point();
+            pointOne.x = int.Parse(Console.ReadLine());
+            pointOne.y = int.Parse(Console.ReadLine());
+            Point pointTwo = new Point();
+            pointTwo.x = int.Parse(Console.ReadLine());
+            pointTwo.y = int.Parse(Console.ReadLine());
+            int CatheusX = 0;
+            int CatheusY = 0;
+            if (pointOne.x > pointTwo.x)
             {
                 MathService service = new MathService();
-                int resultOne = service.GetDistance(point.y, point.x);
-                Console.WriteLine(resultOne);
+                int resultOne = service.GetCathetus(pointTwo.y, pointOne.x);
+                CatheusX = resultOne;
             }
-            if (point.x < point.y)
+            if (pointOne.x < pointTwo.x)
             {
                 MathService service = new MathService();
-                int resultOne = service.GetDistance(point.x, point.y);
-                Console.WriteLine(resultOne);
+                int resultOne = service.GetCathetus(pointOne.x, pointTwo.y);
+                CatheusX = resultOne;
             }
+            if (pointOne.y > pointTwo.y)
+            {
+                MathService service = new MathService();
+                int resultOne = service.GetCathetus(pointTwo.y, pointOne.x);
+                CatheusY = resultOne;
+            }
+            if (pointOne.y < pointTwo.y)
+            {
+                MathService service = new MathService();
+                int resultOne = service.GetCathetus(pointOne.x, pointTwo.y);
+                CatheusY = resultOne;
+            }
+            MathService serviceOne = new MathService();
+            double Hypotenuse = serviceOne.GetHypotenuse(CatheusX, CatheusY);
+            Console.WriteLine(Hypotenuse);
         }
     }
 }
