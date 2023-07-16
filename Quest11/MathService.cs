a1e5f41318eaeaa51e9a58444214f9175a90a336
animeonelove2190 <daniil.davletshin1@gmail.com>
2023-07-17 03:35:32 +0500

    Add 11, switch to Methods2

---
+++ Quest11/MathService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quest11
+{
+    class MathService
+    {
+        public int GetMaxNumberOfTwo(int numOne, int numTwo)
+        {
+            if (numOne >= numTwo)
+            {
+                int numMin = numTwo;
+                numTwo = numOne;
+                numOne = numMin;
+            }
+            int numMax = numTwo;
+            return numMax;
+        }
+    }
+}
---
+++ Quest11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Quest11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOne = int.Parse(Console.ReadLine());
+            int numTwo = int.Parse(Console.ReadLine());
+            MathService serviceMaxNumOfTwo = new MathService();
+            int numMax = serviceMaxNumOfTwo.GetMaxNumberOfTwo(numOne, numTwo);
+            Console.WriteLine(numMax);
+        }
+    }
+}
